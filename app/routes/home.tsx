3bef4246ea0bfaf1f8f09b36c97eae4d6d9a036e
unknown <yalshaeyr@hotmail.com>
2025-03-21 15:35:56 +1100

    remove boilerplate

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,12 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Progress Visualiser" },
+    { name: "description", content: "Welcome to Progress Visualiser!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return "Welcome to Progress Visualiser!";
 }
