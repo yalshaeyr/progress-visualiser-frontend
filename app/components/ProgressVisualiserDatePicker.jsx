ce05e7f8d39d54b3a73f4dcb81716957f5dece70
unknown <yalshaeyr@hotmail.com>
2025-03-31 23:37:17 +1100

    Modify AppBar to use SearchContext

--- app/components/ProgressVisualiserDatePicker.jsx
+++ app/components/ProgressVisualiserDatePicker.jsx
@@ -3,7 +3,7 @@ import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import dayjs from 'dayjs';
 
-export default function MetricDatePicker() {
+export default function ProgressVisualiserDatePicker() {
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
         <DatePicker
