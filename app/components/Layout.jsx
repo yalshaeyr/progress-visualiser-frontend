04c7de3326f10686608622891f5291f7ba78e817
unknown <yalshaeyr@hotmail.com>
2025-04-02 22:03:29 +1100

    Add global notification element to Layout

--- app/components/Layout.jsx
+++ app/components/Layout.jsx
@@ -1,24 +1,29 @@
+import NotificationProvider from "./NotificationProvider";
 import Stack from "@mui/material/Stack";
 import SideBar from "./SideBar";
 import SearchProvider from "./SearchProvider";
 import AppBar from "./AppBar";
+import ProgressVisualiserNotification from "./ProgressVisualiserNotification";
 import { Outlet } from "react-router";
 
 export default function Layout() {
     return (
-        <Stack direction="row">
-            <SideBar />
-            <Stack
-                direction="column"
-                sx={{
-                    flexGrow: 1,
-                }}
-            >
-                <SearchProvider>
-                    <AppBar/>
-                    <Outlet/>
-                </SearchProvider>
+        <NotificationProvider>
+            <Stack direction="row">
+                <SideBar />
+                <Stack
+                    direction="column"
+                    sx={{
+                        flexGrow: 1,
+                    }}
+                >
+                    <SearchProvider>
+                        <AppBar />
+                        <Outlet />
+                    </SearchProvider>
+                </Stack>
             </Stack>
-        </Stack>
-    )
+            <ProgressVisualiserNotification />
+        </NotificationProvider>
+    );
 }
