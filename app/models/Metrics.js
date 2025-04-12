bea48d13b1ebd1e6f53e4fe1169ea083191aa7b3
unknown <yalshaeyr@hotmail.com>
2025-04-12 21:52:11 +1000

    Refactor metrics: move EditToolbar

---
+++ app/models/Metrics.js
@@ -0,0 +1,20 @@
+const MetricsModel = [
+    {
+        field: "name",
+        headerName: "Name",
+        flex: 1,
+        editable: true,
+    },
+    {
+        field: "description",
+        headerName: "Description",
+        flex: 3,
+        editable: true,
+    },
+    {
+        field: "unit",
+        headerName: "Unit",
+        flex: 1,
+        editable: true,
+    },
+];
