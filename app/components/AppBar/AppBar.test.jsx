5c893755eb90f19ea2d952d535b37bb244f98eab
unknown <yalshaeyr@hotmail.com>
2025-04-12 17:06:06 +1000

    Add AppBar unit tests

---
+++ app/components/AppBar/AppBar.test.jsx
@@ -0,0 +1,51 @@
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+import AppBar from "./AppBar";
+import { useSearch } from "../../hooks/useSearch";
+
+jest.mock("../../hooks/useSearch", () => ({
+    useSearch: jest.fn(),
+}));
+
+jest.mock("../BreadcrumbStack", () => () => (
+    <div data-testid="breadcrumb-stack" />
+));
+jest.mock("../ProgressVisualiserForm", () => (props) => (
+    <div
+        data-testid="progress-visualiser-form"
+        onClick={props.onSearch}
+    ></div>
+));
+
+describe("AppBar", () => {
+    it("renders AppBar component with child components", () => {
+        const mockSearchLogic = jest.fn();
+        useSearch.mockReturnValue({ searchLogic: mockSearchLogic });
+
+        render(<AppBar />);
+
+        // Check for BreadcrumbStack
+        const breadcrumbStack = screen.getByTestId("breadcrumb-stack");
+        expect(breadcrumbStack).toBeVisible();
+
+        // Check for ProgressVisualiserForm
+        const progressVisualiserForm = screen.getByTestId(
+            "progress-visualiser-form"
+        );
+        expect(progressVisualiserForm).toBeVisible();
+    });
+
+    it("passes searchLogic to ProgressVisualiserForm", () => {
+        const mockSearchLogic = jest.fn();
+        useSearch.mockReturnValue({ searchLogic: mockSearchLogic });
+
+        render(<AppBar />);
+
+        const progressVisualiserForm = screen.getByTestId(
+            "progress-visualiser-form"
+        );
+        progressVisualiserForm.click();
+
+        expect(mockSearchLogic).toHaveBeenCalled();
+    });
+});
